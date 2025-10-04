d8935ad32ae51f2d31b0a557f0c79ac32e3f4175
Vincentkled <vincentkled27@gmail.com>
2025-10-04 14:02:22 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  }
+};
+
+module.exports = nextConfig;
