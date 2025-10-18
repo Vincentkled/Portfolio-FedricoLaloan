ed239f119c9e56a7720c87621c91bd020e2ff99b
Vincentkled <vincentkled27@gmail.com>
2025-10-18 23:30:18 +0700

    ganti linkedin

--- components/intro.tsx
+++ components/intro.tsx
@@ -99,18 +99,18 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/fedrico-laloan-a51235183/"
+          href="https://www.linkedin.com/in/fedricolaloan/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
-        <a
+        {/* <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
           href="https://github.com/yourusername"
           target="_blank"
         >
           <FaGithubSquare />
-        </a>
+        </a> */}
       </motion.div>
     </section>
   );
